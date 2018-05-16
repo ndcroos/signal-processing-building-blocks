691b9df35d06bf90d24fe44712b9545324ec70e1
ndcroos <ndcroos@users.noreply.github.com>
2018-05-16 12:04:11 -0400

    Update periodic_signal.m

--- periodic_signal.m
+++ periodic_signal.m
@@ -1,3 +1,7 @@
-function [x] = periodic_signal(n, vx, fts, a, phi)
-m = 0:n-1
-return
+function [x] = periodic_signal(n, vx, fts, amps, phis)
+    t = 0:n-1;
+    k = 1:numel(amps); % numel(amps): number of elements of amps
+    x = sum(amps' .* cos(2*pi*t*k*fts + (ones(n,1).*phis')),2);
+    energy_per_sample = mean(x.^2) % https://www.dsprelated.com/freebooks/mdft/Signal_Metrics.html
+    x = x .* sqrt(vx / energy_per_sample);
+end
